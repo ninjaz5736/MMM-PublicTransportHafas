90237049a48f5fbb98fe25a18252e084f0af50b3
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2022-01-23 22:37:23 +0100

    "default-case": "off"

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     sourceType: "module"
   },
   rules: {
+    "default-case": "off",
     "prefer-destructuring": "off",
     "prettier/prettier": "error",
     "import/no-unresolved": "off"
