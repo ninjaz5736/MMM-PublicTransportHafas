cc6cda9d9037fc5f3daa92ecb2fe49c0cbf39737
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2022-02-02 12:35:46 +0100

    Show "plattform" as default Those who do not need it can deactivate it via `tableHeaderOrder`.

--- MMM-PublicTransportHafas.js
+++ MMM-PublicTransportHafas.js
@@ -41,7 +41,7 @@ Module.register("MMM-PublicTransportHafas", {
     showTableHeaders: true,             // Show table headers?
     showTableHeadersAsSymbols: true,    // Table Headers as symbols or written?
     showWarningRemarks: true,           // Show warning remarks?
-    tableHeaderOrder: ["time", "line", "direction"], // In which order should the table headers appear? (add "platform" if you like)
+    tableHeaderOrder: ["time", "line", "direction", "platform" ], // In which order should the table headers appear?
     maxUnreachableDepartures: 0,        // How many unreachable departures should be shown?
     maxReachableDepartures: 7,          // How many reachable departures should be shown?
     fadeUnreachableDepartures: true,
