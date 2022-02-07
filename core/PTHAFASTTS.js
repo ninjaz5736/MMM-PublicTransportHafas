77abe67071149c02d3c98471205c614b83aa8350
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2022-02-07 22:55:40 +0100

    Update

--- core/PTHAFASTTS.js
+++ core/PTHAFASTTS.js
@@ -1,48 +1,60 @@
-"use strict";
-
+/* eslint-disable no-restricted-syntax */
+/* global config */
 // Initialize new SpeechSynthesisUtterance object
-let speech = new SpeechSynthesisUtterance();
+const speech = new SpeechSynthesisUtterance();
 
 // Set speech Language
 speech.lang = config.language;
+
+function sayTime() {
+  const time = new Date();
+  const timeString = `Es ist ${time.getHours()} Uhr ${time.getMinutes()},\n`;
+  speech.text = timeString;
+  speechSynthesis.speak(speech);
+  speech.text = "";
+}
 
 function sayDepartures() {
   // Time
   sayTime();
 
   // Set the text property with the value of the textarea
-  let pthWrappers = document.getElementsByClassName("pthWrapper");
+  const pthWrappers = document.getElementsByClassName("mmm-pth-wrapper");
 
-  for (let pthWrapper of pthWrappers) {
+  for (const pthWrapper of pthWrappers) {
     // Station
-    let station = pthWrapper.getElementsByTagName("header")[0];
-    speech.text = speech.text + "\n. Haltestelle " + station.innerText + ".\n";
+    const station = pthWrapper.getElementsByTagName("header")[0];
+    speech.text = `${speech.text}\n. Haltestelle ${station.innerText}.\n`;
 
-    let pthTable = pthWrapper.getElementsByClassName("pthTable")[0];
+    const pthTable = pthWrapper.getElementsByClassName("mmm-pth-table")[0];
 
     // Line
-    let lines = pthTable.getElementsByClassName("pthSign");
-    for (let line of lines) {
-      line.innerText = "Linie " + line.innerText;
+    const lines = pthTable.getElementsByClassName("mmm-pth-sign");
+    for (const line of lines) {
+      line.innerText = `Linie ${line.innerText}`;
     }
 
     // Direction
-    let directions = pthTable.getElementsByClassName("pthDirectionCell");
-    for (let direction of directions) {
-      direction.innerText = "in Richtung " + direction.innerText;
+    const directions = pthTable.getElementsByClassName(
+      "mmm-pth-direction-cell"
+    );
+    for (const direction of directions) {
+      direction.innerText = `in Richtung ${direction.innerText}`;
     }
 
     // Platform
-    let platforms = pthTable.getElementsByClassName("pthPlatformCell");
-    for (let platform of platforms) {
+    const platforms = pthTable.getElementsByClassName("mmm-pth-platform-cell");
+    // eslint-disable-next-line no-restricted-syntax
+    for (const platform of platforms) {
       if (platform.innerText !== "") {
-        platform.innerText = "Steig " + platform.innerText;
+        platform.innerText = `Steig ${platform.innerText}`;
       }
     }
 
-    let rows = pthTable.getElementsByTagName("tr");
-    for (let row of rows) {
-      speech.text = speech.text + row.innerText.replaceAll("\n", " ") + ",\n";
+    const rows = pthTable.getElementsByTagName("tr");
+    // eslint-disable-next-line no-restricted-syntax
+    for (const row of rows) {
+      speech.text = `${speech.text + row.innerText.replaceAll("\n", " ")},\n`;
       speech.text = speech.text.replaceAll("str.", "straße");
       speech.text = speech.text.replaceAll("Str.", "Straße");
       speech.text = speech.text.replaceAll("STR.", "Straße");
@@ -57,26 +69,19 @@ function sayDepartures() {
   speechSynthesis.speak(speech);
 }
 
-let speechHello = new SpeechSynthesisUtterance();
+const speechHello = new SpeechSynthesisUtterance();
 speechHello.lang = config.language;
 
-speechHello.addEventListener("end", function (event) {
+speechHello.addEventListener("end", () => {
+  speechSynthesis.cancel();
   sayTime();
+  sayDepartures();
 });
 
 function sayHello() {
-  let hello = "Guten Tag!";
+  const hello = "Guten Tag!";
   speechHello.text = hello;
   speechSynthesis.speak(speechHello);
-}
-
-function sayTime() {
-  let time = new Date();
-  let timeString =
-    "Es ist " + time.getHours() + " Uhr " + time.getMinutes() + ",\n";
-  speech.text = timeString;
-  speechSynthesis.speak(speech);
-  speech.text = "";
 }
 
 sayHello();
