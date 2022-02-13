3aac90df7e6c0abd33ff15f5cc6c2b96b950e884
Kristjan SCHMIDT <35647502+KristjanESPERANTO@users.noreply.github.com>
2022-02-13 02:34:46 +0100

    Update

--- core/PTHAFASTTS.js
+++ core/PTHAFASTTS.js
@@ -1,37 +1,50 @@
 /* eslint-disable no-restricted-syntax */
-/* global config */
+/* global config Log */
 // Initialize new SpeechSynthesisUtterance object
-const speech = new SpeechSynthesisUtterance();
+const speechHello = new SpeechSynthesisUtterance();
 
 // Set speech Language
-speech.lang = config.language;
+speechHello.lang = config.language;
 
-function sayTime() {
+function getTimeAnnouncementString() {
   const time = new Date();
-  const timeString = `Es ist ${time.getHours()} Uhr ${time.getMinutes()},\n`;
-  speech.text = timeString;
-  speechSynthesis.speak(speech);
-  speech.text = "";
+  const timeString = `Es ist ${time.getHours()} Uhr ${time.getMinutes()}.\n`;
+  return timeString;
 }
 
-function sayDepartures() {
+function speak(text) {
+  speechSynthesis.cancel();
+  speechHello.text = text;
+  Log.log(text);
+  speechSynthesis.speak(speechHello);
+}
+
+function getDesparturesString() {
+  let allDesparturesString;
+
   // Time
-  sayTime();
+  allDesparturesString = getTimeAnnouncementString();
 
   // Set the text property with the value of the textarea
   const pthWrappers = document.getElementsByClassName("mmm-pth-wrapper");
 
   for (const pthWrapper of pthWrappers) {
-    // Station
-    const station = pthWrapper.getElementsByTagName("header")[0];
-    speech.text = `${speech.text}\n. Haltestelle ${station.innerText}.\n`;
+    const pthWrapperClone = pthWrapper.cloneNode(true);
 
-    const pthTable = pthWrapper.getElementsByClassName("mmm-pth-table")[0];
+    // Station
+    const station = pthWrapperClone.getElementsByTagName("header")[0];
+    allDesparturesString += `\nHaltestelle ${station.innerText}.\n`;
+
+    const pthTable = pthWrapperClone.getElementsByClassName("mmm-pth-table")[0];
+    const thead = pthWrapperClone.querySelector("thead");
+    if (thead !== null) {
+      thead.remove();
+    }
 
     // Line
     const lines = pthTable.getElementsByClassName("mmm-pth-sign");
     for (const line of lines) {
-      line.innerText = `Linie ${line.innerText}`;
+      line.innerText = `Linie ${line.innerText} `;
     }
 
     // Direction
@@ -39,49 +52,66 @@ function sayDepartures() {
       "mmm-pth-direction-cell"
     );
     for (const direction of directions) {
-      direction.innerText = `in Richtung ${direction.innerText}`;
+      direction.innerText = `in Richtung ${direction.innerText} `;
     }
 
     // Platform
     const platforms = pthTable.getElementsByClassName("mmm-pth-platform-cell");
-    // eslint-disable-next-line no-restricted-syntax
+
     for (const platform of platforms) {
       if (platform.innerText !== "") {
-        platform.innerText = `Steig ${platform.innerText}`;
+        platform.innerText = ` von Steig ${platform.innerText}`;
       }
     }
 
     const rows = pthTable.getElementsByTagName("tr");
-    // eslint-disable-next-line no-restricted-syntax
-    for (const row of rows) {
-      speech.text = `${speech.text + row.innerText.replaceAll("\n", " ")},\n`;
-      speech.text = speech.text.replaceAll("str.", "straße");
-      speech.text = speech.text.replaceAll("Str.", "Straße");
-      speech.text = speech.text.replaceAll("STR.", "Straße");
-    }
+    allDesparturesString += `Es gibt ${rows.length} Abfahrten.\n`;
 
-    speech.text = speech.text.replaceAll("\t", " ").replaceAll("  ", " ");
+    if (rows.length > 0) {
+      let departureCounter = 0;
+
+      for (const row of rows) {
+        let departureString;
+        if (row.innerText.indexOf("⚠️") > -1) {
+          departureString = row.innerText.split("⚠️")[1];
+        } else {
+          departureCounter += 1;
+          departureString = `Abfahrt ${departureCounter}: ${row.innerText}`;
+        }
+        departureString = departureString
+          .replaceAll("\n", " ")
+          .replaceAll("str.", "straße")
+          .replaceAll("Str.", "Straße")
+          .replaceAll("STR.", "Straße");
+        allDesparturesString += `${departureString}.\n`;
+      }
+
+      allDesparturesString = allDesparturesString
+        .replaceAll("\t", " ")
+        .replaceAll("  ", " ");
+    }
   }
 
-  console.log(speech.text);
-
   // Start Speaking
-  speechSynthesis.speak(speech);
+  return allDesparturesString;
 }
 
-const speechHello = new SpeechSynthesisUtterance();
-speechHello.lang = config.language;
-
-speechHello.addEventListener("end", () => {
-  speechSynthesis.cancel();
-  sayTime();
-  sayDepartures();
-});
-
-function sayHello() {
-  const hello = "Guten Tag!";
-  speechHello.text = hello;
-  speechSynthesis.speak(speechHello);
+function getGreetingString() {
+  const time = new Date();
+  const hour = time.getHours();
+  let greetingsString;
+  if (hour > 18) {
+    greetingsString = "Guten Abend. ";
+  } else if (hour > 0 < 10) {
+    greetingsString = "Guten Morgen. ";
+  } else if (hour > 10 < 18) {
+    greetingsString = "Guten Tag. ";
+  }
+  return greetingsString;
 }
 
-sayHello();
+setTimeout(() => {
+  let firstTextToSpeech = getGreetingString();
+  firstTextToSpeech += getDesparturesString();
+  speak(firstTextToSpeech);
+}, 5000);
